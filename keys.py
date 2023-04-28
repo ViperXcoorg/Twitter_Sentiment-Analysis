fda7b85b5ad89de0d761f2b53d86566657192e34
Prakruthir12 <88709239+Prakruthir12@users.noreply.github.com>
2023-04-28 12:54:52 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,6 +1,6 @@
 # Initializing the Twitter api keys and tokens
 
-api_key = "WAO5yXDSzoMsoSgpA7HFY2Fs2"
-api_key_secret = "WnNGaSuZQD8ZrLFqHJya5Gqoi6Np7cQnXoIadudYNkvpIyrk5h"
-access_token = "3325557223-nsFd2O86yQNfeDrPpvrddJ45PEe1x6GngjEiWIh"
-access_token_secret = "R4LgD6ZBgmNEkoF8kCNR2lKWHM0iELsfKmxck4NQ6GF1Y"
+api_key = 
+api_key_secret = 
+access_token = 
+access_token_secret = 
